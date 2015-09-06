6bcbe925207527b92401ce376e8d8bf8c0385d7d
Egor <sapronov.egor@gmail.com>
2015-09-06 20:36:11 +0600

    fix

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-let app = require('./app');
 let port = process.env.PORT || 3000
-app.listen(port, function () {
+let http = require('./app');
+
+http.listen(port, function () {
 	console.log(`Server listen on ${port}`)
 });
